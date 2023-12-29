9da142e9a4baa94789744d5646d14956efadc272
Bart Sides <bartsides@gmail.com>
2023-12-29 13:26:30 -0500

    Start menu
    Added start menu

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 class_name Enemy
 
 @export var SPEED = 20
-@export var DISTANCE_MARGIN = 2
+@export var DISTANCE_MARGIN = 1
 @export var TOTAL_HEALTH = 10
 
 var health = 10.0
