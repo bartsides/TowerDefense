0e098b202088e493905c04038a59fe600a979f91
Bart Sides <bartsides@gmail.com>
2023-12-30 17:12:00 -0500

    Limit zoom
    Limit how far player can zoom

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,5 +1,7 @@
 extends Camera2D
 
+const MAX_ZOOM = 10
+const MIN_ZOOM = .02
 const ZOOM_INCR = .2
 const MOVE_SPEED = 2
 
@@ -18,7 +20,7 @@ func _process(_delta):
 
 func _input(event):
 	if event.is_action_pressed("mouse_scroll_down"):
-		zoom -= Vector2(ZOOM_INCR, ZOOM_INCR)
+		zoom = Vector2(max(zoom.x - ZOOM_INCR, MIN_ZOOM), max(zoom.y - ZOOM_INCR, MIN_ZOOM))
 
 	if event.is_action_pressed("mouse_scroll_up"):
-		zoom += Vector2(ZOOM_INCR, ZOOM_INCR)
+		zoom = Vector2(min(zoom.x + ZOOM_INCR, MAX_ZOOM), min(zoom.y + ZOOM_INCR, MAX_ZOOM))
